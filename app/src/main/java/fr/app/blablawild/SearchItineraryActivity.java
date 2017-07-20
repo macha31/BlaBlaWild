b315562318e25f8fb4666132232215125138ca24
macha <m.charrier33000@gmail.com>
2017-07-20 12:08:30 +0200

    DatePicker

--- app/src/main/java/fr/app/blablawild/SearchItineraryActivity.java
+++ app/src/main/java/fr/app/blablawild/SearchItineraryActivity.java
@@ -1,14 +1,21 @@
 package fr.app.blablawild;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class SearchItineraryActivity extends AppCompatActivity {
     EditText editTextDeparture;
@@ -21,6 +28,8 @@ public class SearchItineraryActivity extends AppCompatActivity {
     int toastDuration;
 
     public final static String EXTRA_REQUEST = "trip";
+    private static final String TAG = "SearchItineraryActivity";
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +44,36 @@ public class SearchItineraryActivity extends AppCompatActivity {
         context = getApplicationContext();
         toast = Toast.makeText(context, getString(R.string.toast), toastDuration);
         toastDuration = Toast.LENGTH_SHORT;
+
+        editTextDated.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        SearchItineraryActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDateSetListener,
+                        year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+
+            }
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener(){
+            @Override
+            public void onDateSet(DatePicker datePicker, int year , int month, int day){
+                month= month + 1;
+                Log.d(TAG, "onDateSet: mm/dd/yyyy" + month + "/" + day + "/" + year);
+
+                String date = month + "/" + day + "/" + year;
+                editTextDated.setText(date);
+            }
+        };
 
         buttonSearch.setOnClickListener(new View.OnClickListener() {
             @Override
